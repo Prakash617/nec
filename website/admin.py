7de20cfd3d745017bf3831d946aed5d86d6379e0
Prakash Thapa <prakashthapa617@gmail.com>
2023-12-27 22:30:16 +0545

    model added

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+# class GoogleBooksAdmin(admin.ModelAdmin):
+#     list_display = ('bookid', 'category')
+#     search_fields = ('bookid', 'category__name')
+
+# admin.site.register(GoogleBooks, GoogleBooksAdmin)
+
+# class CategoryBookAdmin(admin.ModelAdmin):
+#     list_display = ('name',)
+#     search_fields = ('name',)
+
+admin.site.register([Course, Category, Sub_Category,Quiz,Question,Choice])
--- website/models.py
+++ website/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    course = models.ForeignKey(Course, null=True, blank=True,on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.name
+    
+class Sub_Category(models.Model):
+    category = models.ForeignKey(Category, null=True, blank=True,on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=100)
+    more_sub = models.TextField(blank=True)
+    details = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    
+class Quiz(models.Model):
+    course = models.ForeignKey(Course, null=True, blank=True,on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=100)
+    public =  models.BooleanField(default=False)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+    
