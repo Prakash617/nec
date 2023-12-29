b78be46d222d0fb099521083e80ba82f9b3ac5a3
Prakash Thapa <prakashthapa617@gmail.com>
2023-12-29 23:47:56 +0545

    some arragement

--- website/urls.py
+++ website/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', home, name="admin_page"),
     path('superuser', superuser, name="superuser"),
     path('slogin', superuser_login, name="slogin"),
+    # path('nec', nec_page, name="nec_page"),
+    path('<str:course_name>/', course, name='course'),
+
     path('superuser/logout', superuser_logout, name="superuser_logout"),
 
     
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from .models import *
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
@@ -9,7 +10,7 @@ from django.contrib.auth.decorators import login_required,user_passes_test
 
 # Create your views here.
 def home(request):
-    template_path = 'index.html'
+    template_path = 'home.html'
     context = {}
     
     return render(request, template_path, context)
@@ -50,3 +51,26 @@ def superuser_login(request):
 def superuser_logout(request):
     logout(request)
     return redirect("slogin")
+
+def course(request,course_name):
+    template_path = 'index.html'
+    context = {}
+    
+    # Query all categories and their related sub-categories
+    categories = Category.objects.filter(course__name=course_name)
+    
+    sub_categories = Sub_Category.objects.all()
+    # Loop through each category and retrieve its related sub-categories
+    for category in categories:
+        subcategories = Sub_Category.objects.filter(category=category)
+        subcategory_list = [{'name': sub.name, 'more_sub': sub.more_sub, 'details': sub.details} for sub in subcategories]
+        
+        # Add the category and its subcategories to the dictionary
+        context[category.name] = subcategory_list
+        
+    print(context)
+        
+    return render(request, template_path, {"context": context})
+
+    
+    
