16e396fd9b12344cdd5df86f201fc8d0ef0e2914
Prakash617 <prakashthapa617@gmail.com>
2024-01-06 19:07:52 +0545

    added some data

--- nec/settings.py
+++ nec/settings.py
@@ -111,6 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
+# TIME_ZONE = 'America/NewYork
 
 USE_I18N = True
 
--- website/views.py
+++ website/views.py
@@ -19,14 +19,98 @@ def home(request):
 
 def populate_subcategories():
     context = {}
+
     context['1. Concept of Basic Electrical and Electronics Engineering'] = [
         '1.1 Basic concept',
         '1.2 Network theorems',
         '1.3 Alternating current fundamentals',
         '1.4 Semiconductor devices',
         '1.5 Signal generator',
-        '1.6 Amplifiers:',
+        '1.6 Amplifiers'
     ]
+
+    context['2. Digital Logic and Microprocessor'] = [
+        '2.1 Digital logic',
+        '2.2 Combinational and arithmetic circuits',
+        '2.3 Sequential logic circuit',
+        '2.4 Microprocessor',
+        '2.5 Microprocessor system',
+        '2.6 Interrupt operations'
+    ]
+
+    context['3. Programming Language and Its Applications'] = [
+        '3.1 Introduction to C programming',
+        '3.2 Pointers, structure and data files in C programming',
+        '3.3 C++ language constructs with objects and classes',
+        '3.4 Features of object-oriented programming',
+        '3.5 Pure virtual function and file handling'
+        '3.6 Generic programming and exception handling'
+    ]
+
+    context['4. Computer Organization and Embedded System'] = [
+        '4.1 Control and central processing units',
+        '4.2 Control and central processing units',
+        '4.3 Input-Output organization and multiprocessor',
+        '4.4 Hardware-Software design issues on embedded system',
+        '4.5 Real-Time operating and control system',
+        '4.6 Hardware descripts language and IC technology',
+        
+    ]
+
+    context['5. Concept of Computer Network and Network Security System'] = [
+        '5.1  Introduction to computer networks and physical layer',
+        '5.2 Data link layer',
+        '5.3 Network layer',
+        '5.4 Transport layer',
+        '5.5 Application layer',
+        '5.6 Network security',
+    ]
+
+    context['6. Theory of Computation and Computer Graphics'] = [
+        '6.1 Introduction to finite automata',
+        '6.2 Introduction to context free language',
+        '6.3 Turing machine',
+        '6.4 Introduction of computer graphics',
+        '6.5 Two-dimensional transformation',
+        '6.6 Three-dimensional transformation'
+    ]
+
+    context['7. Data Structures and Algorithm, Database System and Operating System'] = [
+        '7.1 Introduction to data structure, list, linked lists and trees',
+        '7.2 Sorting, searching, and graphs',
+        '7.3 Introduction to data models, normalization, and SQL',
+        '7.4 Transaction processing, concurrency control and crash recovery',
+        '7.5 Introduction to Operating System and process management',
+        '7.6 Memory management, file systems and system administration',
+    ]
+
+    context['8. Software Engineering and Object-Oriented Analysis & Design'] = [
+        '8.1 Software process and requirements',
+        '8.2 Software design',
+        '8.3 Software testing, cost estimation, quality management, and configuration management',
+        '8.4 Object-oriented fundamentals and analysis',
+        '8.5 Object-oriented design',
+        '8.6 Object-oriented design implementation',
+    ]
+
+    context['9. Artificial Intelligence and Neural Network'] = [
+        '9.1 Introduction to AI and intelligent agent',
+        '9.2 Problem solving and searching techniques',
+        '9.3 Knowledge representation',
+        '9.4 Expert system and natural language processing',
+        '9.5 Machine learning',
+        '9.6  Neural networks',
+    ]
+
+    context['10. Project Planning, Design and Implementation'] = [
+        '10.1 Engineering drawings and its concepts',
+        '10.2 Engineering Economics',
+        '10.3 Project planning and scheduling',
+        '10.4 Project management',
+        '10.5 Engineering professional practice',
+        '10.6 Engineering Regulatory Body',
+    ]
+
 
     for category_name, subcategory_list in context.items():
         category, created = Category.objects.get_or_create(name=category_name)
